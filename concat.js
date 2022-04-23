24ad2ff8b23641bd05504f2e10b7678ca85d4556
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-04-23 20:18:44 +0300

    first-Tasks

---
+++ concat.js
@@ -0,0 +1,7 @@
+function concat(input){
+    let fname = input[0];
+    let lname= input[1];
+    let age = Number(input[2]);
+    let city = input[3];
+    console.log(`You are ${fname} ${lname}, a ${age}-years old person from ${city}.`)
+}
---
+++ project.js
@@ -0,0 +1,6 @@
+function calculate(input){
+    let name = input[0];
+    let projects = Number(input[1]);
+    console.log(`The architect ${name} will need ${projects*3} hours to complete ${projects} project/s.`)
+
+}
