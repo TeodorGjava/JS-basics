96af7ec302fed959e03bfdb53e1c9b1ca29d2295
TeodorGjava <90547780+TeodorGjava@users.noreply.github.com>
2022-04-24 11:56:11 +0300

    Update yardGardening.js

--- yardGardening.js
+++ yardGardening.js
@@ -1,6 +1,7 @@
 function fun(input){
     let squareMeters = Number(input[0]);
     let price = (squareMeters *7.61);
-    let discount = price *0.82;
-    console.log(`The final price is: ${discount.toFixed(2)} lv.\nThe discount is: ${(price - discount).toFixed(2)} lv.`)
+    let discount = price *0.18;
+    let priceWithDiscount = price-discount;
+    console.log(`The final price is: ${priceWithDiscount.toFixed(2)} lv.\nThe discount is: ${ discount.toFixed(2)} lv.`)
 }
